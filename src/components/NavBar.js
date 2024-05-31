34e6778d4605511924f1fb1b264c85a793cffa2c
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-05-31 19:15:07 +0700

    Add NavBar and CV Download

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import './NavBar.css';
+import React from "react";
+import { Link } from 'react-scroll';
+import { FaDownload } from 'react-icons/fa';
+
+const Navbar = () => {
+    return(
+        <nav className='navbar'>
+            <div className='navbar-Menu'>
+                <Link className='Menu-ListItem'>Home</Link>
+                <Link className='Menu-ListItem'>About</Link>
+                <Link className='Menu-ListItem'>Skills</Link>
+                <Link className='Menu-ListItem'>Projects</Link>
+            </div>
+            <a href="/CV_NGUYEN-CAO-THANG.pdf" download>
+                <button className='Menu-Btn'>
+                    <FaDownload className='Menu-Btn-Img'/>Download CV
+                </button>
+            </a>
+        </nav>
+    )
+}
+
+export default Navbar;
