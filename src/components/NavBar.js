d856e1368a589ecce49d3c0967517dd42ab5f7d7
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-06-03 23:37:23 +0700

    Upd Navbar Rps v1.1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,15 @@ import { Link } from 'react-scroll';
 import { FaDownload } from 'react-icons/fa';
 
 const Navbar = () => {
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = '/CV_NGUYEN-CAO-THANG.pdf';
+        link.download = 'CV_NGUYEN-CAO-THANG.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return(
         <nav className='navbar'>
             <div className='navbar-Menu'>
@@ -12,11 +21,9 @@ const Navbar = () => {
                 <Link className='Menu-ListItem'>Skills</Link>
                 <Link className='Menu-ListItem'>Projects</Link>
             </div>
-            <a href="/CV_NGUYEN-CAO-THANG.pdf" download>
-                <button className='Menu-Btn'>
-                    <FaDownload className='Menu-Btn-Img'/>Download CV
-                </button>
-            </a>
+            <button className='Menu-Btn' onClick={handleDownload}>
+                <FaDownload className='Menu-Btn-Img'/>Download CV
+            </button>
         </nav>
     )
 }
