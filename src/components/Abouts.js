2b00f506149de7dd96c38e416e7685f0824d2b90
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-06-08 00:12:15 +0700

    Update Intro Animation and Icon Navigation

--- src/components/Abouts.js
+++ src/components/Abouts.js
@@ -4,12 +4,17 @@ import './Abouts.css';
 const Abouts = () => {
   return (
     <div id="intro">
-        <div className='intro-Content'>
-            <div className='welcome-container'>
-              <span className='welcome' data-text="Welcome!!!">Welcome!!!</span>
-            </div>
+        <div className='abouts-Container'>
+          <div className='welcome-container'>
+            <span className='welcome' data-text="Welcome !!!">Welcome !!!</span>
+          </div>
+          <div className='intro-Content'>
             <p>My name is Nguyen Cao Thang, I'm a front-end developer based in Ho Chi Minh City, Viet Nam.</p>
             <p>I'm passionate about cutting-edge, pixel-perfect, beautiful interfaces and intuitively implemented UX.</p>
+            <div className='intro-Icon-Container'>
+
+            </div>
+          </div>
         </div>
     </div>
   )
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import './Intro.css';
 import { FaFacebook, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { BiLogoGmail } from "react-icons/bi";
 
 const Intro = () => {
   return (
     <div id="intro">
         <div className='intro-Container'>
+          <div className='welcome-container'>
+            <span className='welcome' data-text="Welcome !!!">Welcome !!!</span>
+          </div>
           <div className='intro-Content'>
-            <div className='welcome-container'>
-              <span className='welcome' data-text="Welcome!!!">Welcome!!!</span>
-            </div>
             <p>My name is Nguyen Cao Thang, I'm a front-end developer based in Ho Chi Minh City, Viet Nam.</p>
             <p>I'm passionate about cutting-edge, pixel-perfect, beautiful interfaces and intuitively implemented UX.</p>
-            <div>
-              <button className='intro-Icon'><FaFacebook></FaFacebook></button>
-              <button className='intro-Icon'><FaGithub></FaGithub></button>
-              <button className='intro-Icon'><FaLinkedin></FaLinkedin></button>
+            <div className='intro-Icon-Container'>
+              <button className='intro-Icon'>
+                <a href="https://facebook.com/Shu.Dwayne" target="_blank" rel="noopener noreferrer">
+                  <FaFacebook/>
+                </a>
+              </button>
+              <button className='intro-Icon'><FaGithub/></button>
+              <button className='intro-Icon'><FaLinkedin/></button>
+              <button className='intro-Icon'><BiLogoGmail/></button>
             </div>
           </div>
         </div>
