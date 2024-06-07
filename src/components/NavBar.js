796116c060daa64fbbd4d097c563ef2479bd3969
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-06-08 00:01:15 +0700

    Update Navbar v1.3 and Scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,57 @@
 import './NavBar.css';
-import React from "react";
+import React, {useState} from "react";
 import { Link } from 'react-scroll';
 import { FaDownload } from 'react-icons/fa';
 
 
 const Navbar = () => {
+    const [currentComponent, setCurrentComponent] = useState('Home');
     return(
-        <nav className='navbar'>
-            <div className='navbar-Menu'>
-                <Link to="/" className="Menu-ListItem">Home</Link>
-                <Link to="/about" className="Menu-ListItem">About</Link>
-                <Link to="/projects" className="Menu-ListItem">Projects</Link>
-            </div>
-            <a href="/portfolio/CV_NGUYEN-CAO-THANG.pdf" download className='Menu-Btn'>
-                <FaDownload className='Menu-Btn-Img'/>Download CV
-            </a>
-        </nav>
+        <header>
+            <nav className='navbar'>
+                <div className='navbar-Menu'>
+                    <Link
+                        activeClass="active"
+                        to="Home"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        className={currentComponent === 'Home' ? 'Menu-ListItem active' : 'Menu-ListItem'}
+                        onClick={() => setCurrentComponent('Home')}
+                    >
+                        Home
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="Abouts"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        className={currentComponent === 'Abouts' ? 'Menu-ListItem active' : 'Menu-ListItem'}
+                        onClick={() => setCurrentComponent('Abouts')}
+                    >
+                        Abouts
+                    </Link>
+                    <Link
+                        activeClass="active"
+                        to="Projects"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                        className={currentComponent === 'Projects' ? 'Menu-ListItem active' : 'Menu-ListItem'}
+                        onClick={() => setCurrentComponent('Projects')}
+                    >
+                        Projects
+                    </Link>
+                </div>
+                <a href="/portfolio/CV_NGUYEN-CAO-THANG.pdf" download className='Menu-Btn'>
+                    <FaDownload className='Menu-Btn-Img'/>Resume
+                </a>
+            </nav>
+        </header>
     )
 }
 
