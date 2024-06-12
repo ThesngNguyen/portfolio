367e99fb8196a7ab462ab280e9a02592a7967cbf
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-06-12 17:52:56 +0700

    Update Abouts Component UI v1.3

--- src/components/Abouts.js
+++ src/components/Abouts.js
@@ -1,43 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './Abouts.css';
-import Carousel from 'react-bootstrap/Carousel';
 import image1 from '../assets/images/image1.jpg';
+import image2 from '../assets/images/image2.jpg';
+import image3 from '../assets/images/image3.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Abouts = () => {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
   return (
     <div id="abouts">
-        <div className='abouts-Container'>
-          <Carousel activeIndex={index} onSelect={handleSelect} className='carousel-Container'>
-            <Carousel.Item>
-              <img
-                className="carousel-Image"
-                src={image1}
-                alt="First slide"
-              />
-              <Carousel.Caption className='carousel-Caption'>
-                <h3>First slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="carousel-Image"
-                src={image1}
-                alt="Second slide"
-              />
-              <Carousel.Caption className='carousel-Caption'>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+      <div className='abouts-Component'>
+        <div className='title-Abouts'>
+          <h1>Abouts</h1>
+          <p>These are some of my dope memories from back when I was grindin' and hustlin' with my amazing crew, tryin' to smash our goals as college homies.<br></br> Feelin' blessed to have such a tight fam to ride with on this wild journey.</p>
         </div>
+        <div className='slider-Container'>
+          <div className='slide slider-1'>
+            <img src={image1} alt="Google Solution Challenge 2022" />
+            <h5 className='slide-Description'>Google Solution Challenge 2022</h5>/
+          </div>
+          <div className='slide slider-2'>
+            <img src={image2} alt="Hoa Sen University TechDev Challenge 2023" />
+            <h5 className='slide-Description'>Hoa Sen University TechDev Challenge 2023</h5>
+          </div>
+          <div className='slide slider-3'>
+            <img src={image3} alt="Sponsored Hoa Sen Champion - Season 1" />
+            <h5 className='slide-Description'>Sponsored Hoa Sen Champion - Season 1</h5>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
