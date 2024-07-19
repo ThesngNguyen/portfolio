3ea3f79fce03a6070d90eb055f03759965b173b1
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-07-19 18:27:48 +0700

    Component: Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import React from 'react';
 
-
+import Sidebar from "./components/Sidebar/Sidebar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Services from "./components/Services/Services";
+import Skill from "./components/Skill/Skill";
+import Project from "./components/Project/Project";
+import Contact from "./components/Contact/Contact";
 
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Sidebar></Sidebar>
+      <main className="home">
+        <Home></Home>
+        <About></About>
+        <Experience></Experience>
+        <Services></Services>
+        <Skill></Skill>
+        <Project></Project>
+        <Contact></Contact>
+      </main>
+    </div>
   );
 }
 
