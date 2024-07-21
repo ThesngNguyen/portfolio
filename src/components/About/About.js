7354ce46d569a672df4a573896e7713602a88524
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-07-21 17:04:09 +0700

    About: Fix Grammar LOL xD

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,9 +8,9 @@ function About() {
       <div className="about__container grid">
         <div className="about__data grid">
           <div className="about__info">
-            <p className="about__description">I am a web developer from Vietnam, and you can call me Theng, which is my nickname on social media and in games. </p>
-            <p className="about__description">I am studying Software Engineering and expect to graduate at the end of 2024. I am also interested in the Internet of Things and Machine Learning. Currently, I am self-studying and improving my programming skills with help from club members and experts at workshops held at my school. </p>
-            <p className="about__description">My goal is to become a Fullstack Web Developer and DevOps specialist. I am in the process of learning these skills on my own.</p>
+            <p className="about__description">Hi there! I'm Thang but you can also call me Theng, a web developer from Vietnam. This nickname is used on any social media platforms and in games.</p>
+            <p className="about__description">I'm currently studying Software Engineering and will have hoped to graduate by the end of 2024. Besides , I'm really interested in the Internet of Things and Machine Learning. now, I'm self-studying and working to improve my programming skills with some helps from club members and experts at workshops held at my school.</p>
+            <p className="about__description">My ultimate goal is to become a Fullstack Web Developer and a DevOps specialist. I'm actively learning these stuff on my own.</p>
           </div>
         </div>
         <img src='https://emoji.discadia.com/emojis/c74ea7f7-2a99-4051-a1e7-424e39892900.gif' alt='' className='about__gif'></img>
