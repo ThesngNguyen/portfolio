54dbdf33fa3b6a70b9da9cc47394ba09f40865e8
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-07-23 20:00:42 +0700

    Skill: Finish Layoutt

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,9 +1,84 @@
 import React from 'react'
 import "./Skill.css"
+import technologyData from "../../data/technologies.json";
+import TechnologyBox from './Technology/TechnologyBox';
 
 function Skill() {
   return (
-    <div>Skill</div>
+    <section className="skill container section" id="skill">
+      <h2 className="section__title">Allow me to display my confident</h2>
+      <div className="skill__container grid">
+        <div className='skill__name'>
+          <p>Library / Framework</p>
+        </div>
+        {technologyData.filter((value) => value.techCategory === "Library/Framework")
+        .map((value, techID) => (
+          <>
+          <TechnologyBox
+            key = {techID}
+            icon = {value.techIcon}
+            title = {value.techName}
+            src = {value.techURL}
+          />
+          </>
+        ))
+        }
+      </div>
+
+      <div className="skill__container grid">
+        <div className='skill__name'>
+          <p>Languages</p>
+        </div>
+        {technologyData.filter((value) => value.techCategory === "Languages")
+        .map((value, techID) => (
+          <>
+          <TechnologyBox
+            key = {techID}
+            icon = {value.techIcon}
+            title = {value.techName}
+            src = {value.techURL}
+          />
+          </>
+        ))
+        }
+      </div>
+
+      <div className="skill__container grid">
+        <div className='skill__name'>
+          <p>Database</p>
+        </div>
+        {technologyData.filter((value) => value.techCategory === "Databases")
+        .map((value, techID) => (
+          <>
+          <TechnologyBox
+            key = {techID}
+            icon = {value.techIcon}
+            title = {value.techName}
+            src = {value.techURL}
+          />
+          </>
+        ))
+        }
+      </div>
+
+      <div className="skill__container grid">
+        <div className='skill__name'>
+          <p>Tools / Platforms</p>
+        </div>
+        {technologyData.filter((value) => value.techCategory === "Tools/Platforms")
+        .map((value, techID) => (
+          <>
+          <TechnologyBox
+            key = {techID}
+            icon = {value.techIcon}
+            title = {value.techName}
+            src = {value.techURL}
+          />
+          </>
+        ))
+        }
+      </div>
+    </section>
   )
 }
 
