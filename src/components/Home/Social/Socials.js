0c34bd7b524dc85d2d8c53dc63c4c0619921f7de
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-07-25 17:52:40 +0700

    Socials: Update Gmail Icon

--- src/components/Home/Social/Socials.js
+++ src/components/Home/Social/Socials.js
@@ -5,27 +5,27 @@ const Socials = () => {
   return (
     <div className="home__socials">
         <a href="facebook.com/Shu.Dwayne" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i class="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-facebook"></i>
         </a>
 
         <a href="https://github.com/ThesngNguyen" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i class="fa-brands fa-github"></i>
+            <i className="fa-brands fa-github"></i>
         </a>
 
         <a href="https://www.linkedin.com/in/williamnguyeenx/" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i class="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-linkedin"></i>
         </a>
 
         <a href="https://dev.to/williamnguyeenx" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i class="fa-brands fa-dev"></i>
+            <i className="fa-brands fa-dev"></i>
         </a>
 
         <a href="https://discordapp.com/users/315123170006925312 " className="home__social-link" target='_blank'  rel="noreferrer">
-            <i class="fa-brands fa-discord"></i>
+            <i className="fa-brands fa-discord"></i>
         </a>
 
         <a href="mailto:ncthang151002@gmail.com" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i class="fa-solid fa-envelope"></i>
+            <i className="fa-brands fa-google"></i>
         </a>
     </div>
   )
