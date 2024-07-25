2f55a27a428aa80dd59f09084e338a9c0618664c
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-07-25 17:50:46 +0700

    Project: Add Modal Session

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import "./Project.css"
+import projectData from "../../data/projects.json";
+import ProjectBox from './ProjectBox/ProjectBox';
 
 function Project() {
   return (
-    <div>Project</div>
+    <section className="project container section" id="project">
+      <h2 className="section__title">Check out what I’ve built</h2>
+      <div className="project__container grid">
+        <div className='project__title'>
+          <h3>Here are only my featured projects.</h3>
+        </div>
+        <div className='project__list'>
+          {
+            projectData.map((value, projectID) => {
+              return(
+                <ProjectBox
+                  key = {projectID}
+                  projectTitle = {value.projectTitle}
+                  projectImage = {value.projectImage}
+                  projectDescription = {value.projectDescription}
+                  projectTechnologies = {value.projectTechnologies}
+                  projectSource = {value.projectSource}
+                  projectTeamSize = {value.projectTeamSize}
+                  projectRole = {value.projectRole}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
