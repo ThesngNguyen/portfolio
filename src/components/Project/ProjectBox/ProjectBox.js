584055bc3ee3b75c9ec78c4d3a39282efa38933d
Nguyễn Cao Thắng <ncthang151002@gmail.com>
2024-07-25 17:46:48 +0700

    Project: Add Project Modal

---
+++ src/components/Project/ProjectBox/ProjectBox.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import "./ProjectBox.css";
+import { getImageUrl } from '../../../data/utils';
+
+const ProjectBox = (props) => {
+    const [toggleState, setToggleState] = useState(0);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+  return (
+    <div className="project__item">
+        <h3>{props.projectTitle}</h3>
+        <ul className='project__tech'>
+          {props.projectTechnologies.map((technology, index) => (
+            <li key={index}>{technology}</li>
+          ))}
+        </ul>
+        <p>{props.projectDescription}</p>
+        <div className="project__link">
+            <a href={props.projectSource} className='project__icon' target='_blank' rel='noreferrer'> 
+                <i className="fa-brands fa-github"></i>
+            </a>
+
+            <span className="project__button" onClick={() => toggleTab(1)}>View More {" "}
+                <i class="fa-solid fa-arrow-right project__button-icon"></i>
+            </span>
+
+            <div className={toggleState === 1 ? "project__modal active-modal" : "project__modal"}>
+                <div className="project__modal-content">
+                    <i class="fa-solid fa-circle-xmark project__modal-close" onClick={() => toggleTab(0)}></i>
+                    <h3 className="project__modal-title">{props.projectTitle}</h3>
+                    <img src={getImageUrl(props.projectImage)} alt="" className="project__modal-image"/>
+                    <p className="project__modal-description">{props.projectDescription}</p>
+                    <p>Team Size : {props.projectTeamSize}</p>
+                    <p>Role : {props.projectRole}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectBox
