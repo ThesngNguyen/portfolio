39d2494e38b0bd260d603b6e392f87d0efe0a323
Nguyễn Cao Thắng <thangnc@whammytech.com>
2025-01-05 00:33:51 +0700

    feat: remove facebook social button

--- src/components/Home/Social/Socials.js
+++ src/components/Home/Social/Socials.js
@@ -2,33 +2,29 @@ import React from 'react';
 import "./Socials.css";
 
 const Socials = () => {
-  return (
-    <div className="home__socials">
-        <a href="facebook.com/Shu.Dwayne" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i className="fa-brands fa-facebook"></i>
-        </a>
+    return (
+        <div className="home__socials">
+            <a href="https://github.com/ThesngNguyen" className="home__social-link" target='_blank' rel="noreferrer">
+                <i className="fa-brands fa-github"></i>
+            </a>
 
-        <a href="https://github.com/ThesngNguyen" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i className="fa-brands fa-github"></i>
-        </a>
+            <a href="https://www.linkedin.com/in/williamnguyeenx/" className="home__social-link" target='_blank' rel="noreferrer">
+                <i className="fa-brands fa-linkedin"></i>
+            </a>
 
-        <a href="https://www.linkedin.com/in/williamnguyeenx/" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i className="fa-brands fa-linkedin"></i>
-        </a>
+            <a href="https://dev.to/williamnguyeenx" className="home__social-link" target='_blank' rel="noreferrer">
+                <i className="fa-brands fa-dev"></i>
+            </a>
 
-        <a href="https://dev.to/williamnguyeenx" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i className="fa-brands fa-dev"></i>
-        </a>
+            <a href="https://discordapp.com/users/315123170006925312 " className="home__social-link" target='_blank' rel="noreferrer">
+                <i className="fa-brands fa-discord"></i>
+            </a>
 
-        <a href="https://discordapp.com/users/315123170006925312 " className="home__social-link" target='_blank'  rel="noreferrer">
-            <i className="fa-brands fa-discord"></i>
-        </a>
-
-        <a href="mailto:ncthang151002@gmail.com" className="home__social-link" target='_blank'  rel="noreferrer">
-            <i className="fa-brands fa-google"></i>
-        </a>
-    </div>
-  )
+            <a href="mailto:ncthang151002@gmail.com" className="home__social-link" target='_blank' rel="noreferrer">
+                <i className="fa-brands fa-google"></i>
+            </a>
+        </div>
+    )
 }
 
 export default Socials
